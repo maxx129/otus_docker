3c91ac1026da76ec2d4bad614d065f3113aa1d92
m.artyukhov <m.artyukhov@sksbank.ru>
2025-04-23 13:47:53 +0300

    Working with docker

---
+++ src/main/java/com/example/docker_test/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.docker_test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @GetMapping
+    public String greeting() {
+        return "Hello World!!!";
+    }
+}
